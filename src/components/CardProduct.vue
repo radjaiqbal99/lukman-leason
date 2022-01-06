22b70c4cc78a4b6a6f913184cf571895480bd4f6
Radja Iqbal Sanusi Hrp <sanusiiqbal36@gmail.com>
2022-01-06 23:36:34 +0700

    learning

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,31 +1,29 @@
 <template>
-    
-  <div class="card-product">
-    <div class="card" style="width: 18rem;">
-      <!-- <img :src="'../assets/images/' + product.gambar " class="card-img-top" alt="..."> -->
-      <div class="card-tittle border">
-
-        <h4 class="m-2">{{product.name}}</h4>
-      </div>
-      <div class="card-body">
-        <p class="card-text text-left"><strong>Type: </strong>{{product.type}}</p>
-        <p class="card-text text-left"><strong>Purchased at: </strong>{{product.purchased_at}}</p>
-        <p class="card-text text-left"><strong>Tags: </strong>{{product.tags[0]}}, {{product.tags[1]}}, {{product.tags[2]}} </p>
-        <p class="text-left"><strong>Placement :</strong></p>
-        <p class="card-text text-left">-Room No : {{product.placement.room_id}}</p>
-        <p class="card-text text-left">-Name : {{product.placement.name}}</p>
-
-        
-        <!-- <router-link :to="'/foods/'+product.id" href="#" class="btn btn-success float-right">Pesan</router-link> -->
+  <div>
+    <div class="card-product">
+      <div v-for="(data, index) in product" :key="index">
+        <div v-if="data.type == target || target === ''" class="card" style="width: 18rem;">
+          <div class="card-tittle border">
+            <h4 class="m-2">{{data.name}}</h4>
+          </div>
+          <div class="card-body">
+            <p class="card-text text-left"><strong>Type: </strong>{{data.type}}</p>
+            <p class="card-text text-left"><strong>Purchased at: </strong>{{data.purchased_at}}</p>
+            <p class="card-text text-left"><strong>Tags: </strong>{{data.tags[0]}}, {{data.tags[1]}}, {{data.tags[2]}} </p>
+            <p class="text-left"><strong>Placement :</strong></p>
+            <p class="card-text text-left">-Room No : {{data.placement.room_id}}</p>
+            <p class="card-text text-left">-Name : {{data.placement.name}}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
   name: "CardProduct",
-  props: ["product"]
+  props: ["product","target"]
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,27 @@
   <div class="home">
     <h2>JSON Manipulation</h2>
     <div class="container">
+      <button @click="target = ''">All</button>
+      <button @click="target = 'furniture'">Furniture</button>
+      <button @click="target = 'electronic'">Elektronic</button>
       <div class="row mb-3 mx-auto justify-content-center">
-        <div class="col-md-4 mt-4" v-for="product in product" :key="product.inventory_id">
-          <CardProduct :product="product"/>
-        </div>
+        <!-- <div class="col-md-4 mt-4" v-for="(data,index) in product" :key="index"> -->
+          <!-- <CardProduct :product="data"/> -->
+          <!-- <CardProduct v-if="data.type == 'furniture'" :product="data"/> -->
+          <CardProduct :target="target" :product="product"/>
+        <!-- </div> -->
       </div>
+      <!-- <h1>Elektronic</h1>
       <div class="row mb-3 mx-auto justify-content-center">
-        <div class="col-md-4 mt-4" >
-          <FilterElectronic :filterElectronic="filterElectronic" />
+        <div class="col-md-4 mt-4" v-for="(data,index) in product" :key="index">
+          <CardProduct v-if="data.type == 'electronic' " :product="data"/>
         </div>
-      </div>
+      </div> -->
+      <!-- <div class="row mb-3 mx-auto justify-content-center">
+        <div class="col-md-4 mt-4" v-for="(data,index) in product" :key="index">
+          <FilterElectronic v-if="data.type == 'electronic' " :filterElectronic="filterElectronic" />
+        </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -19,24 +30,30 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+// import dataJson from '../../public/db.json'
 import axios from "axios";
 import CardProduct from "@/components/CardProduct.vue";
-import FilterElectronic from "@/components/FilterElectronic.vue";
+// import FilterElectronic from "@/components/FilterElectronic.vue";
 
 export default {
   name: "Home",
   components: {
     // HelloWorld
-    CardProduct,
-    FilterElectronic
+    CardProduct
+    // FilterElectronic
   },
+
   data() {
     return {
-      product: [],
-      filterElectronic: []
+      product: null,
+      filterElectronic: [],
+      target : ''
     };
   },
   methods: {
+    change(payload){
+      this.target = payload
+    },
     setProduct(data) {
       this.product = data;
     }
@@ -59,7 +76,7 @@ export default {
   },
   mounted() {
     axios
-      .get("http://localhost:3000/data")
+      .get("db.json")
       // .then(function(response) {
       //   console.log(response);
       // })
@@ -67,6 +84,7 @@ export default {
       //   console.log(error);
       // });
       .then(response => this.setProduct(response.data))
+      // .then(response => console.log(response))
       .catch(error => console.log("Gagal:", error));
   },
 
